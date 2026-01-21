23ed89305f9ed2bd67bd10f8ff0710fe4d0e5e6c
Tayler <mhmodshhade230@gmail.com>
2026-01-21 11:41:27 +0200

    Create a new component called Specialist

---
+++ my-app/app/components/Specialists/Specialist.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Specialist.scss"
+
+const Specialist = () => {
+    return (
+        <section className="Specialist-section">
+           ttt
+        </section>
+    );
+};
+
+export default Specialist;
