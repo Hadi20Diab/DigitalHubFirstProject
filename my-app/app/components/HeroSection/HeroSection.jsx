e9bf8e6ebb9f75574ebcd79cb8197a3fe84e5291
Hadi Diab <103230009+Hadi20Diab@users.noreply.github.com>
2026-01-21 10:15:26 +0200

    style: add HeroSection component with initial structure for future enhancements

---
+++ my-app/app/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./HeroSection.css"
+
+const HeroSection = () => {
+    return (
+        <section>
+            <div></div>
+            <div>
+                <div className="col-3">
+                    <h3>24/7</h3>
+                    <p>Online Support</p>
+                </div>
+                <div className="col-3">
+                    <h3>24/7</h3>
+                    <p>Online Support</p>
+                </div>
+                <div className="col-3">
+                    <h3>24/7</h3>
+                    <p>Online Support</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HeroSection;
