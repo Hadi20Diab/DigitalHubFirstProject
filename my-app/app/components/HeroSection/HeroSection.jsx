1b70e64c76066e1dbef47ee8b3b06d21037319e7
Hadi Diab <103230009+Hadi20Diab@users.noreply.github.com>
2026-01-21 10:32:07 +0200

    style: replace HeroSection.css with HeroSection.scss and update layout structure

--- my-app/app/components/HeroSection/HeroSection.jsx
+++ my-app/app/components/HeroSection/HeroSection.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import "./HeroSection.css"
+import "./HeroSection.scss"
 
 const HeroSection = () => {
     return (
-        <section>
+        <section className="hero-section">
             <div></div>
-            <div>
+            <div className="row-features">
                 <div className="col-3">
                     <h3>24/7</h3>
                     <p>Online Support</p>
                 </div>
+                <span className="vertical-line"></span>
                 <div className="col-3">
                     <h3>24/7</h3>
                     <p>Online Support</p>
                 </div>
+                <span className="vertical-line"></span>
                 <div className="col-3">
                     <h3>24/7</h3>
                     <p>Online Support</p>
