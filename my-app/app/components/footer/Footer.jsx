06c143915acb718db14bde6b9f82a6178934ad37
Tayler <mhmodshhade230@gmail.com>
2026-01-21 11:33:11 +0200

    add the image to the footer & update styles

--- my-app/app/components/footer/Footer.jsx
+++ my-app/app/components/footer/Footer.jsx
@@ -8,26 +8,26 @@ const Footer = () => {
             <div className="footer-content">
                 <div className="footer-logo">
                     <div className="footer-brand-name-and-text">
-                        <h3>E-sheba</h3>
+                        <h2>E-sheba</h2>
                         <p className="desc1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec risus feugiat lectus risus sed ullamcorper. Auctor semper fermentum</p>
                         <p className="desc2">volutpat integer vel. In rhoncus elementum nunc, malesuada mi sed. Nibh est sit lobortis id semper.</p>
                     </div>
                     <div className="footer-social-media">
-                        <a href=""><i className="bi bi-facebook"></i></a>
-                        <a href=""><i className="bi bi-instagram"></i></a>
-                        <a href=""><i className="bi bi-twitter"></i></a>
+                        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="bi bi-facebook"></i></a>
+                        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><i className="bi bi-instagram"></i></a>
+                        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="bi bi-twitter"></i></a>
                     </div>
                 </div>
                 <div className="footer-links">
-                    <h3 className="links-title">Useful Links</h3>
+                    <h2 className="links-title">Useful Links</h2>
                     <a href="">About Us</a>
                     <a href="">Privacy Policy</a>
                     <a href="">Our Mission</a>
                     <a href="">Our Team</a>
                 </div>
                 <div className="footer-address"> 
-                    <h3 className="address-title">Address</h3>
-                    <img src="#" alt="there is no image" />
+                    <h2 className="address-title">Address</h2>
+                    <a href="https://maps.app.goo.gl/7mzAXH2oQG7JzNpQA" target="_blank" rel="noopener noreferrer"><img src="./Rectangle 18.png" alt="there is no image" /></a>
                 </div>
             </div>
             <div className="footer-copyright">
