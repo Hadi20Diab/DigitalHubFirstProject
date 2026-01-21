86e6e6b29046a21bb0223f60af03d0e4502f42ae
Tayler <mhmodshhade230@gmail.com>
2026-01-21 13:06:11 +0200

    make add choose us section and start with adding content in it

---
+++ my-app/app/components/ChooseUs/ChooseUs.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./ChooseUs.scss"
+
+const ChooseUs = () => {
+    return (
+        <section className="ChooseUs-section">
+            <img src="./Rectangle 11.png" alt="Why Choose Us" />
+           <div className="ChooseUs-text">
+              <h1 className="ChooseUS-title">Why You Choose Us?</h1>
+              <div className="ChooseUs-points">
+                <div className="ChooseUs-point">
+                    <a href=""><li></li></a>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className="ChooseUs-point">
+                    <a href=""><li></li></a>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className="ChooseUs-point">
+                    <a href=""><li></li></a>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className="ChooseUs-point">
+                    <a href=""><li></li></a>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+              </div>
+           <a href="">learn more</a>
+           </div>
+        </section>
+    );
+};
+
+export default ChooseUs;
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
+import ChooseUs from "./components/ChooseUs/ChooseUs";
 import Specialist from "./components/Specialists/Specialist";
 
 export default function Home() {
   return (
     <>
     <Specialist />
+    <ChooseUs />
     </>
   );
 }
