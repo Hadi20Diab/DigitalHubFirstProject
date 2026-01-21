ff4e0e8ecec9e7cc6cf939384dcf0c304ac77f02
Hadi Diab <103230009+Hadi20Diab@users.noreply.github.com>
2026-01-21 14:19:32 +0200

    feat: add OurMembers component and integrate it into the homepage

---
+++ my-app/app/components/OurMembers/OurMembers.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import "./OurMembers.scss"
+
+const OurMembers = () => {
+
+    return (
+        <section className="OurMembersContainer">
+            <h1>sass</h1>
+        </section>
+    );
+};
+
+export default OurMembers;
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,13 +1,15 @@
 "use client";
 
 import ChooseUs from "./components/ChooseUs/ChooseUs";
+import OurMembers from "./components/OurMembers/OurMembers";
 import Specialist from "./components/Specialists/Specialist";
 
 export default function Home() {
   return (
     <>
-    <Specialist />
-    <ChooseUs />
+      <Specialist />
+      <ChooseUs />
+      <OurMembers />      
     </>
   );
 }
