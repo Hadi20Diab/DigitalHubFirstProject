a1ec18a5f5e75371f2ca3d898503e5bcecd7b75f
Tayler <mhmodshhade230@gmail.com>
2026-01-21 10:30:53 +0200

    Second Update

--- my-app/app/components/footer/Footer.jsx
+++ my-app/app/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="footer-content">
                 <div className="footer-logo">
                     <div className="footer-brand-name-and-text">
-                        <h2>E-sheba</h2>
+                        <h3>E-sheba</h3>
                         <p className="desc1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec risus feugiat lectus risus sed ullamcorper. Auctor semper fermentum</p>
                         <p className="desc2">volutpat integer vel. In rhoncus elementum nunc, malesuada mi sed. Nibh est sit lobortis id semper.</p>
                     </div>
@@ -18,19 +18,19 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="footer-links">
-                    <h2>Useful Links</h2>
+                    <h3>Useful Links</h3>
                     <a href="">About Us</a>
                     <a href="">Privacy Policy</a>
                     <a href="">Our Mission</a>
                     <a href="">Our Team</a>
                 </div>
                 <div className="footer-address"> 
-                    <h2>Address</h2>
+                    <h3>Address</h3>
                     <img src="#" alt="there is no image" />
                 </div>
             </div>
             <div className="footer-copyright">
-                <hr />
+                <hr style={{width: "85%" , margin: "0 auto", marginTop: "20px", marginBottom: "20px"}}/>
                 <p>© 2026 All Right Reserved.</p>
             </div>
         </div>
